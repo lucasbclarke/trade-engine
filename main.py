58a6b6a97fad1a7e58c6927d12c7c42947c9d136
lucasbclarke <lucasbaileyclarke@outlook.com>
2026-06-01 17:24:57 +1000

    fixed the continuous loop when purchasing a stock, updated the code structure to better handle the continous buying and selling of stocks, ability to have multiple users added

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
+account = [
+    ["Alice", [40], [["SNPS", 40]] ],
+    ["Terry", [60], [] ],
+    ["Joy", [200], [] ]
+]
+
+userIndex = -1
+
 class Portfolio:
 
-    def buy(self, stockName, amount, stockList, accountBalance, stockValue):
-        while True:
-            print(f"purchased {amount} stock")
-            foundStock = False
+    def buy(self, stockName, amount, stockValue, userAccount):
+        accountBalance = userAccount[1]
+        stockList = userAccount[2]
+
+        print(f"purchased {amount} stock")
+        foundStock = False
         
-            for stocks in stockList:
-                if stockName in stocks:
-                    print(f"{stockName} is in stocklist")
-                    stocks[1] = int(stocks[1]) + int(amount)
-                    print(f"stock amount is now {stocks[1]}")
+        for stocks in stockList:
+            print(f"sotcks = {stocks}")
+            if stockName in stocks:
+                print(f"{stockName} is in stocklist")
 
-                    for stock in stockValue:
-                        if stock[0] == stockName:
-                            accountBalance = accountBalance - ( stock[1] * int(amount) )
-                            print(f"accountBalance is now {accountBalance}")
-                            break
-
-                    foundStock = True
-                    break
-                break
-
-            if not foundStock:
-                print(f"{stockName} is not in stocklist")
-                stockList.append([stockName, amount])
                 for stock in stockValue:
                     if stock[0] == stockName:
-                        accountBalance = accountBalance - ( stock[1] * int(amount) )
-                        print(f"accountBalance is now {accountBalance}")
+                        stockPrice = stock[1] * int(amount)
+                        if accountBalance[0] < stockPrice:
+                            print(f"Your accountBalance is only {accountBalance[0]} you can't buy {amount} stock with your current funds")
+                        else:
+                            accountBalance[0] -= stockPrice
+                            stocks[1] = int(stocks[1]) + int(amount)
+                            print(f"stock amount is now {stocks[1]}")
+                            print(f"accountBalance is now {accountBalance[0]}")
+                    break
+                foundStock = True
+                break
+            break
+
+        if not foundStock:
+            print(f"{stockName} is not in stocklist")
+            stockList.append([stockName, amount])
+            for stock in stockValue:
+                if stock[0] == stockName:
+                    accountBalance[0] -= ( stock[1] * int(amount) )
+                    print(f"accountBalance is now {accountBalance[0]}")
 
                 break
 
-    def sell(self, stockName, amount, stockList, accountBalance, stockValue):
+    def sell(self, stockName, amount, stockValue, userAccount):
+        accountBalance = userAccount[1]
+        stockList = userAccount[2]
+
         print(f"sold {amount} stock")
         foundStock = False
         
@@ -42,14 +59,14 @@ class Portfolio:
                 for stock in stockValue:
                     if stock[0] == stockName:
                         if int(amount) < stocks[1]:
-                            accountBalance = accountBalance + ( stock[1] * int(amount) )
-                            print(f"accountBalance is now {accountBalance}")
+                            accountBalance[0] += ( stock[1] * int(amount) )
+                            print(f"accountBalance is now {accountBalance[0]}")
 
                             stocks[1] = int(stocks[1]) - int(amount)
                             print(f"you now have {stocks[1]} of {stocks[0]}")
                         elif int(amount) == stocks[1]:
-                            accountBalance = accountBalance + ( stock[1] * int(amount) )
-                            print(f"accountBalance is now {accountBalance}")
+                            accountBalance[0] += ( stock[1] * int(amount) )
+                            print(f"accountBalance is now {accountBalance[0]}")
                             stockList.remove(stocks)
                         else:
                             print(f"you cannot sell {amount} of {stocks[0]}, as you only have {stocks[1]} {stocks[0]}")
@@ -77,28 +94,36 @@ def main():
             ["AVEN", 60] 
     ]
 
-    stockList = [["SNPS", 40]] 
-
-    accountBalance = 100 # this could be changed into a list if one "user" can have multiple accounts
-    
-    # change this so that the user does not perform one action and then the program exists, update it so the user can buy, sell and evaluate multiple times in one running session
-
     while True:
+        user = input("Who are you?: ")
         action = input("Action: ")
         if action == "exit": break
         company = input("Company: ")
         if action == "evaluate": p.evaluate(company) ; continue
         amount = input ("Amount: ")
-        match action:
-            case "buy":
-                p.buy(company, amount, stockList, accountBalance, stockValue)
-                continue 
-            case "sell":
-                p.sell(company, amount, stockList, accountBalance, stockValue)
-                continue
-            case _:
-                print("Not a valid action")
-        break
+        
+        i = 0
+        for users in account:
+            print(f"user is {user} and users[0] is {users[0]}")
+            if user == users[0]:
+                userIndex = i
+                break
+            i += 1
+
+        if userIndex < 0:
+            print(f"{user} is not an account holder")
+        
+        else:
+            match action:
+                case "buy":
+                    p.buy(company, amount, stockValue, account[userIndex])
+                    continue 
+                case "sell":
+                    p.sell(company, amount, stockValue, account[userIndex])
+                    continue
+                case _:
+                    print("Not a valid action")
+            break
     
     print(f"Stock list = {stockList}")
 
